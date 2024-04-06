90f293edd6995aced7b9d92b7f641fa2fffc8ede
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-04-06 18:42:09 +0800

    Update 约瑟夫环问题.py

--- 约瑟夫环问题.py
+++ 约瑟夫环问题.py
@@ -2,7 +2,7 @@ ls=[x for x in range(1,42)] #生成人员编号1~41的列表ls
 print("依次出列的人员编号是:")
 while True:
     if len(ls)<3:           #幸存者只剩两人时，中断循环
-        break;
+        break
     print(ls[2],end=" ")    #输出淘汰者编号ls[2]
     ls.pop(2)               #淘汰者出列
     lt=ls[0:2]              #报数为1、2的元素，切片方式暂存
